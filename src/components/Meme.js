bcaf4019ad866e16ea8293fde97559aab3592d8d
Fernando Salcedo <fsalcedo55@hotmail.com>
2022-02-21 01:22:05 -0500

    Adding event handlers to meme component

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,23 +1,34 @@
 import React from "react";
+import memesData from "../data/memesData";
 
 export default function Meme() {
+  function getMemeImage() {
+    const memesArray = memesData.data.memes.map((meme) => {
+      return <h1>{meme}</h1>;
+    });
+  }
+
   return (
     <div className="flex flex-col gap-6">
       <div className="flex gap-8">
         <input
           type="text"
           className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md border-2 p-2"
-          placeholder="Shut up"
+          placeholder="Top text"
         />
         <input
           type="text"
           className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md border-2 p-2"
-          placeholder="and take my money"
+          placeholder="Bottom text"
         />
       </div>
-      <button className="bg-gradient-to-r from-[#672280] to-[#A626D3] w-full h-10 rounded-md drop-shadow-lg text-white">
+      <button
+        className="bg-gradient-to-r from-[#672280] to-[#A626D3] w-full h-10 rounded-md drop-shadow-lg text-white"
+        onClick={getMemeImage}
+      >
         Get a new meme image 🖼
       </button>
+      <div>{memesArray}</div>
     </div>
   );
 }
